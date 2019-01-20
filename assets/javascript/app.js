dd219d1698c5c23577dc43b95e6a1981576423f6
Kenny <nels5410@gmail.com>
2019-01-19 19:08:44 -0600

    gifs have now been added on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,6 +27,22 @@ $( document ).ready(function(){
             .then(function(response) {
                 console.log(queryURL);
                 console.log(response);
+
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var carDiv = $('<div>');
+                    var p = $('<p>').text("Rating: " + results[i].rating);
+                    var carImage = $('<img>');
+
+                    carImage.attr("src", results[i].images.fixed_height.url);
+
+                    carDiv.append(p);
+                    carDiv.append(carImage);
+
+                    $('#images').append(carDiv);
+
+                }
             
             });
     })
