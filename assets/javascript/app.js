59bd5a925c27ad037882ef0f6737c96ea82de798
Kenny <nels5410@gmail.com>
2019-01-19 18:30:53 -0600

    api logic has been added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-//$( document ).ready(function(){
+$( document ).ready(function(){
     //This is the array of car brands
     var topics = ["acura", "audi", "bmw", "dodge" ,"ford" ,"jeep" ,"lamborghini" ,"nissan" ,"tesla" ,"toyota" ,"volkswagen"];
     console.log(topics);
@@ -14,5 +14,23 @@
         a.text(topics[i]);
         $('#topics-view').append(a);
     }
-//})
+
+    //Giphy API Variable click function
+    $('.topics').on("click", function() {
+        var car = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=tiA8TWDUyhiEmjQ99eKe8WgUlRWF0y9p&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function(response) {
+                console.log(queryURL);
+                console.log(response);
+            
+            });
+    })
+        
+
+})
 
